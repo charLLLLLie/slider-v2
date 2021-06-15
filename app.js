af809c764781e0ccc38aaa32aa8f9324e218351e
charLLLLLie <mega.joker00@gmail.com>
2021-06-16 00:19:48 +0300

    created slider

---
+++ app.js
@@ -0,0 +1,37 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+
+const container = document.querySelector('.container')
+const mainSlide = document.querySelector('.main-slide')
+
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+let activeSliderIndex = 0
+sidebar.style.top = `-${(slidesCount -1) * 100}vh`
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down')
+})
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSliderIndex++
+        if (activeSliderIndex === slidesCount) {
+            activeSliderIndex = 0
+        }
+    } else if (direction === 'down') {
+        activeSliderIndex--
+        if (activeSliderIndex < 0) {
+            activeSliderIndex = slidesCount - 1
+        }
+    }
+
+    const height = container.clientHeight
+    mainSlide.style.transform = `translateY(-${activeSliderIndex*height}px)`
+    sidebar.style.transform = `translateY(${activeSliderIndex*height}px)`
+}
